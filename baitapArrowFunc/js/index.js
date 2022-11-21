fb35377aac055c6c326bf9e48444c7ea63129d05
Nhat Minh <minhtran30994@gmail.com>
2022-11-21 17:20:00 +0700

    hoan thanh es6 buoi 1

---
+++ baitapArrowFunc/js/index.js
@@ -0,0 +1,38 @@
+const domId = (id) => document.getElementById(id);
+
+const ColorList = [
+  "pallet",
+  "viridian",
+  "pewter",
+  "cerulean",
+  "vermillion",
+  "lavender",
+  "celadon",
+  "saffron",
+  "fuschia",
+  "cerulean",
+];
+let content = "";
+for (let i in ColorList) {
+  content += `<button class="color-button ${ColorList[i]}"></button>`;
+}
+domId("colorContainer").innerHTML = content;
+const button = document.getElementsByClassName("color-button");
+for (let i in ColorList) {
+  button[i].onclick = () => {
+    let addClass = button[i].classList[1];
+    // console.log(addClass);
+    domId("house").classList.add(`${addClass}`);
+    for (let j in ColorList) {
+      if (button[j].classList[2]) {
+        button[j].classList.remove("active");
+      }
+    }
+    button[i].classList.add(`active`);
+    if (domId("house").classList[2]) {
+      let removeClass = domId("house").classList[1];
+      domId("house").classList.remove(`${removeClass}`);
+    }
+  };
+}
+document.querySelector(".pallet").classList.add("active");
